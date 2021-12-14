1985a2068acd654351a0f80e1aef90cf477d0454
AlexIstra1989 <alex888fcsm@gmail.com>
2021-12-14 17:50:08 +0300

    added footer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,26 @@
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
+$(document).ready(function() {
+ $('.hero__inner').slick ({
+  dots: true,
+  arrows: false,
+  fade: true,
+  autoplay: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+ });
+});
+
+var acc = document.getElementsByClassName("faq__accordion-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "flex") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "flex";
+    }
+  });
+}
+
--- src/assets/js/slick.js
+++ src/assets/js/slick.js
@@ -3009,32 +3009,3 @@
     };
 
 }));
-
-$(document).ready(function() {
- $('.hero__inner').slick ({
-  dots: true,
-  arrows: false,
-  fade: true,
-  autoplay: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
- });
-});
-
-var acc = document.getElementsByClassName("faq__accordion-btn");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "flex") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "flex";
-    }
-  });
-}
-
-
-//# sourceMappingURL=script.js.map
